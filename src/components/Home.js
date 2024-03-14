e76fef5b533c60f88cc1b4ea03e44c923df96182
GUNTUKU NARESH <guntuku001@gmail.com>
2024-03-14 10:32:51 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,11 +8,11 @@ import { Link, useNavigate } from "react-router-dom";
 
 function Home() {
 	let history = useNavigate();
-	function setID(id, name, price,category) {
+	function setID(id, name, price, category) {
 		localStorage.setItem("id", id);
 		localStorage.setItem("Name", name);
 		localStorage.setItem("Price", price);
-		localStorage.setItem("Category",category);
+		localStorage.setItem("Category", category);
 	}
 	function deleted(id) {
 		let index = array
@@ -24,63 +24,73 @@ function Home() {
 		history("/home");
 	}
 	return (
-		<div style={{ margin: "3rem" }}>
-			<Table striped bordered hover size="sm">
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Price</th>
-						<th>Category</th>
-						<th>Quantity</th>
-					</tr>
-				</thead>
-				<tbody>
-					{array.map((item) => {
-						return (
+		<div className="container-fluid" style={{ margin: "0rem" }}>
+			<div className="row">
+				<div className="col">
+					<Table striped bordered hover size="lg">
+						<thead>
 							<tr>
-								<td>{item.Name}</td>
-								<td>{item.Price}</td>
-								<td>{item.Category}</td>
-								<td>{item.Quantity}</td>	
-								<td>
-									<Link to={`/edit`}>
-										<Button
-											onClick={(e) =>
-												setID(
-													item.id,
-													item.Name,
-													item.Price,
-													item.Category,
-													item.Quantity
-												)
-											}
-											variant="info"
-										>
-											Update
-										</Button>
-									</Link>
-								</td>
-								<td>
-									<Button
-										onClick={(e) =>
-											deleted(item.id)
-										}
-										variant="danger"
-									>
-										Delete
-									</Button>
-								</td>
+								<th>Name</th>
+								<th>Price</th>
+								<th>Category</th>
+								<th>Quantity</th>
 							</tr>
-						);
-					})}
-				</tbody>
-			</Table>
-			<Link className="d-grid gap-3" to="/create">
-				<Button variant="warning" size="lg">
-					Add New Product
-				</Button>
-			</Link>
-		</div>
+						</thead>
+						<tbody>
+							{array.map((item) => {
+								return (
+									<tr>
+										<td>{item.Name}</td>
+										<td>{item.Price}</td>
+										<td>{item.Category}</td>
+										<td>{item.Quantity}</td>
+										<td>
+											<Link to={`/edit`}>
+												<Button
+													onClick={(e) =>
+														setID(
+															item.id,
+															item.Name,
+															item.Price,
+															item.Category,
+															item.Quantity
+														)
+													}
+													variant="info"
+												>
+													Update
+												</Button>
+											</Link>
+										</td>
+										<td>
+											<Button
+												onClick={(e) =>
+													deleted(item.id)
+												}
+												variant="danger"
+											>
+												Delete
+											</Button>
+										</td>
+									</tr>
+								);
+							})}
+						</tbody>
+					</Table>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col">
+					<div className="text-center">
+						<Link to="/create">
+							<Button variant="warning" size="lg">
+								Add New Product
+							</Button>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div >
 	);
 }
 
